e6ef95998e0c869129023312a71bd29cc22452b6
Sam Minot <sminot@fredhutch.org>
2022-11-22 12:19:53 -0800

    Point to visium entrypoint

--- ._wb/tool/visium/run.sh
+++ ._wb/tool/visium/run.sh
@@ -11,7 +11,7 @@ echo
 echo Starting workflow
 nextflow \
     run \
-    "${TOOL_REPO}" \
+    "${TOOL_REPO}/visium.nf" \
     --output_dir "${PWD}" \
     -params-file ._wb/tool/params.json \
     -with-dag \
